c9c242abbad57082da2a45675a4698f2f9c38d7d
Сергей Кухаронак <Tyuiu.KuharonakSV@outlook.com>
2025-09-17 19:36:55 +0500

    Task1

---
+++ Tyuiu.KuharonakSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Сергей";
+        int age = 18;
+        Console.WriteLine("Здраствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
